32a08f14b4424663e45bb0b6426537589ded3e68
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2024-12-15 02:19:38 +0200

    add token display on home page

--- server/handlers/home.go
+++ server/handlers/home.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/OsakiTsukiko/frogpond/server/database"
+	sgl "github.com/OsakiTsukiko/frogpond/server/singleton"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,8 +22,32 @@ func HomeGET(c *gin.Context) {
 		"email":    email,
 	}) */
 
+	user, err := database.GetUserByUsername(username, sgl.DATABASE)
+	if err != nil {
+		c.HTML(http.StatusOK, "error.html", gin.H{
+			"error": "Error loading USER from DATABASE!",
+		})
+		return
+	}
+
+	if user == nil {
+		c.HTML(http.StatusOK, "error.html", gin.H{
+			"error": "User not in DATABASE!!?",
+		})
+		return
+	}
+
+	tokens, err := database.GetUserTokens(user.ID, sgl.DATABASE)
+	if err != nil {
+		c.HTML(http.StatusOK, "error.html", gin.H{
+			"error": "Error getting tokens for user!",
+		})
+		return
+	}
+
 	c.HTML(http.StatusOK, "home.html", gin.H{
 		"username": username,
 		"email":    email,
+		"tokens":   tokens,
 	})
 }
